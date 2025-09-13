0c08bc3da17783ab33b80dd6480132ce7700db36
riaapatel28 <kakadiyariya03@gmail.com>
2025-09-13 10:17:46 +0530

    Update pra_14_1.c

--- pra_14_1.c
+++ pra_14_1.c
@@ -1,6 +1,6 @@
 /*aim:in ATM machine
-editor:krisha mangukiya
-date:3th august,2025*/
+editor:riya kakadiya
+date:12th sapt,2025*/
 #include<stdio.h>
 #include<conio.h>
 void main()
@@ -53,3 +53,4 @@ void main()
 }while(choice!=3);
 
 }
+
