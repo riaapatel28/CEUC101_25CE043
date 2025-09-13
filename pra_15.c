645185a4002dc4b697f7b6f1282dd43c049cc802
riaapatel28 <kakadiyariya03@gmail.com>
2025-09-13 10:21:45 +0530

    Update pra_15.c

--- pra_15.c
+++ pra_15.c
@@ -1,7 +1,7 @@
 /*aim:add a timer
 editor:riya kakadiya
 date:12nd sapt,2025*/
-/*#include<stdio.h>
+#include<stdio.h>
 #include<dos.h>
 #include<conio.h>
 void main()
@@ -15,22 +15,4 @@ void main()
         printf("timer:%d seconds\n",i);
 		delay(1000);
 	}
-}*/
-/*aim:add a timer
-editor:krisha mangukiya
-date:22nd aug,2025*/
-#include<stdio.h>
-#include<dos.h>
-#include<conio.h>
-void main()
-{
-	int i,n;
-
-	printf("enter the seconds=");
-	scanf("%d",&n);
-	for(i=n;i>=0;i--)
-	{
-
-		printf("timer:%d seconds\n",i);
-		delay(1000);
-	}
+}
